db747b4a6e56aac12802070d5df46132f808bbe5
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-05-07 16:10:23 +0500

    feat: add function for transform data from Api
    Data json from server will transforming in object with data whom we choice.

--- src/js/service/swapi-service.js
+++ src/js/service/swapi-service.js
@@ -4,6 +4,11 @@ export default class SwapiService {
         this.imageUrl = 'https://starwars-visualguide.com/assets/img';
 
         this.getResource = this.getResource.bind(this);
+
+        this.transformPeople = this.transformPeople.bind(this);
+        this.transformPlanet = this.transformPlanet.bind(this);
+        this.transformStarship = this.transformStarship.bind(this);
+        this.transformVehicles = this.transformVehicles.bind(this);
     }
 
     async getResource(path, page = 1) {
@@ -14,5 +19,65 @@ export default class SwapiService {
         }
 
         return res.json();
+    }
+
+    extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    transformPeople(person) {
+        return {
+            id: this.extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birth_year,
+            hairColor: person.hair_color,
+            eyeColor: person.eye_color,
+            height: person.height,
+            mass: person.mass,
+            homeworld: person.homeworld,
+            starships: person.starships,
+            films: person.films,
+        };
+    }
+
+    transformPlanet(planet) {
+        return {
+            id: this.extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter,
+        };
+    }
+
+    transformStarship(starship) {
+        return {
+            id: this.extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.cost_in_credits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargo_capacity,
+        };
+    }
+
+    transformVehicles(vehicles) {
+        return {
+            id: this.extractId(vehicles),
+            name: vehicles.name,
+            model: vehicles.model,
+            manufacturer: vehicles.manufacturer,
+            costInCredits: vehicles.cost_in_credits,
+            speed: vehicles.speed,
+            length: vehicles.length,
+            crew: vehicles.crew,
+            passengers: vehicles.passengers,
+            cargoCapacity: vehicles.cargo_capacity,
+        };
     }
 }
