bee2df312125512650f37948edd9b5581ca11091
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-05-08 10:39:11 +0500

    add item details component
    Item details will take a data and generate character information.
    In body if we using children-component, we can choose which information about character will render.

---
+++ src/js/components/item-details/item-details.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './item-details.scss';
+
+const propTypesItemDetails = {
+    item: PropTypes.shape({
+        // person
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+
+        birthYear: PropTypes.string,
+        eyeColor: PropTypes.string,
+        gender: PropTypes.string,
+        hairColor: PropTypes.string,
+        height: PropTypes.string,
+        mass: PropTypes.string,
+    }).isRequired,
+    getImage: PropTypes.func.isRequired,
+    children: PropTypes.node,
+};
+
+const defaultPropsItemDetails = {
+    children: [],
+};
+
+const propTypesItemDetailsChildren = {
+    label: PropTypes.string.isRequired,
+
+    item: PropTypes.shape({
+        // person
+        id: PropTypes.string,
+        name: PropTypes.string,
+
+        birthYear: PropTypes.string,
+        eyeColor: PropTypes.string,
+        gender: PropTypes.string,
+        hairColor: PropTypes.string,
+        height: PropTypes.string,
+        mass: PropTypes.string,
+    }),
+    field: PropTypes.string,
+};
+
+const defaultPropsItemDetailsChildren = {
+    item: {},
+    field: '',
+};
+
+function ItemDetailsChildren({ item, field, label }) {
+    return (
+        <li className='item-details__list-item'>
+            {`${label}:`}
+            <p className='item-details__desc'>{item[field]}</p>
+        </li>
+    );
+}
+
+function ItemDetails(props) {
+    const { item, getImage, children } = props;
+    const { id, name } = item;
+
+    if (!name) {
+        return <p>Please selected item from a list</p>;
+    }
+
+    return (
+        <div className='item-details'>
+            <img
+                className='item-details__img'
+                src={getImage(id)}
+                alt={name}
+            />
+            <div className='item-details__box'>
+                <h2 className='item-details__title'>{name}</h2>
+                <ul className='item-details__list'>
+                    {React.Children.map(children, (child) =>
+                        React.cloneElement(child, { item })
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+ItemDetails.propTypes = propTypesItemDetails;
+ItemDetails.defaultProps = defaultPropsItemDetails;
+ItemDetailsChildren.propTypes = propTypesItemDetailsChildren;
+ItemDetailsChildren.defaultProps = defaultPropsItemDetailsChildren;
+
+export default ItemDetails;
+export { ItemDetailsChildren };
