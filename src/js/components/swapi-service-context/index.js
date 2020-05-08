a153e97f466ed48461e953b970bf69b3d199a70b
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-05-08 11:09:55 +0500

    feat: add context
    Context value is swapi service.
    That will allow deep components use a service, and render
    on a page information, whatever data their will take.

---
+++ src/js/components/swapi-service-context/index.js
@@ -0,0 +1,3 @@
+import SwapiProvider from './swapi-service-context';
+
+export default SwapiProvider;
---
+++ src/js/components/swapi-service-context/swapi-service-context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SwapiProvider = React.createContext();
+
+export default SwapiProvider;
