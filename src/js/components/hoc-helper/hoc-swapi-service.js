963b925f09cce213fcfcd3acdb28158bae567d2d
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-05-09 14:03:24 +0500

    feat: add hoc for swapi service
    That function will determine, what kind type a data take a server,
    and send result to components, whom working with data.

---
+++ src/js/components/hoc-helper/hoc-swapi-service.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import SwapiProvider from '../swapi-service-context';
+
+const hocSwapiService = (mapService) => (Wrapped) => {
+    return function hocSwapiServiceWrap(props) {
+        hocSwapiServiceWrap.propTypes = {
+            itemId: PropTypes.string,
+            updateDetails: PropTypes.func,
+        };
+        hocSwapiServiceWrap.defaultProps = {
+            itemId: '',
+            updateDetails: () => {},
+        };
+
+        const { itemId, updateDetails } = props;
+        const context = useContext(SwapiProvider);
+        const serviceProps = mapService(context);
+        const { getData, getImage } = serviceProps;
+
+        return (
+            <Wrapped
+                itemId={itemId}
+                updateDetails={updateDetails}
+                getData={getData}
+                getImage={getImage}
+            />
+        );
+    };
+};
+
+export default hocSwapiService;
