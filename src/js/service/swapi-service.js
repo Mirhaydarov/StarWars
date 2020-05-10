6bc9c866afb2daf716380fc23f634b69991c6000
Radmir Mirhaydarov <Mirhaydarov@protonmail.com>
2020-05-10 13:24:20 +0500

    refactor: change method to take image from service
    Added fetch() for take an image,
    as result we can take an error from server and handle it.

--- src/js/service/swapi-service.js
+++ src/js/service/swapi-service.js
@@ -1,25 +1,24 @@
+import imageNotFound from './image/image-not-found.webp';
+
 export default class SwapiService {
     constructor() {
         this.url = 'https://swapi.dev/api';
         this.imageUrl = 'https://starwars-visualguide.com/assets/img';
 
         this.getResource = this.getResource.bind(this);
+        this.getImageResource = this.getImageResource.bind(this);
 
         this.getAllPeople = this.getAllPeople.bind(this);
         this.getPerson = this.getPerson.bind(this);
-        this.getPersonImage = this.getPersonImage.bind(this);
 
         this.getAllPlanets = this.getAllPlanets.bind(this);
         this.getPlanet = this.getPlanet.bind(this);
-        this.getPlanetImage = this.getPlanetImage.bind(this);
 
         this.getAllVehicles = this.getAllVehicles.bind(this);
         this.getVehicles = this.getVehicles.bind(this);
-        this.getVehiclesImage = this.getVehiclesImage.bind(this);
 
         this.getAllStarships = this.getAllStarships.bind(this);
         this.getStarship = this.getStarship.bind(this);
-        this.getStarshipImage = this.getStarshipImage.bind(this);
 
         this.transformPeople = this.transformPeople.bind(this);
         this.transformPlanet = this.transformPlanet.bind(this);
@@ -31,10 +30,22 @@ export default class SwapiService {
         const res = await fetch(`${this.url}/${path}/?page=${page}`);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${path}, received ${res.status}`);
+            throw new Error(
+                `Could not fetch ${path}, received ${res.status}`
+            );
         }
 
         return res.json();
+    }
+
+    async getImageResource(path) {
+        const res = await fetch(`${this.imageUrl}/${path}.jpg`);
+
+        if (!res.ok) {
+            return imageNotFound;
+        }
+
+        return res;
     }
 
     async getAllPeople() {
@@ -44,11 +55,14 @@ export default class SwapiService {
 
     async getPerson(id) {
         const res = await this.getResource(`people/${id}`);
-        return this.transformPeople(res);
-    }
+        const imageRes = await this.getImageResource(
+            `characters/${id}`
+        );
 
-    getPersonImage(id) {
-        return `${this.imageUrl}/characters/${id}.jpg`;
+        return {
+            getItem: this.transformPeople(res),
+            getImg: imageRes,
+        };
     }
 
     async getAllPlanets() {
@@ -58,11 +72,12 @@ export default class SwapiService {
 
     async getPlanet(id) {
         const res = await this.getResource(`planets/${id}`);
-        return this.transformPlanet(res);
-    }
+        const imageRes = await this.getImageResource(`planets/${id}`);
 
-    getPlanetImage(id) {
-        return `${this.imageUrl}/planets/${id}.jpg`;
+        return {
+            getItem: this.transformPlanet(res),
+            getImg: imageRes,
+        };
     }
 
     async getAllStarships() {
@@ -72,11 +87,14 @@ export default class SwapiService {
 
     async getStarship(id) {
         const res = await this.getResource(`starships/${id}`);
-        return this.transformStarship(res);
-    }
+        const imageRes = await this.getImageResource(
+            `starships/${id}`
+        );
 
-    getStarshipImage(id) {
-        return `${this.imageUrl}/starships/${id}.jpg`;
+        return {
+            getItem: this.transformStarship(res),
+            getImg: imageRes,
+        };
     }
 
     async getAllVehicles() {
@@ -86,11 +104,14 @@ export default class SwapiService {
 
     async getVehicles(id) {
         const res = await this.getResource(`vehicles/${id}`);
-        return this.transformVehicles(res);
-    }
+        const imageRes = await this.getImageResource(
+            `vehicles/${id}`
+        );
 
-    getVehiclesImage(id) {
-        return `${this.imageUrl}/vehicles/${id}.jpg`;
+        return {
+            getItem: this.transformVehicles(res),
+            getImg: imageRes,
+        };
     }
 
     extractId(item) {
@@ -142,7 +163,7 @@ export default class SwapiService {
             model: vehicles.model,
             manufacturer: vehicles.manufacturer,
             costInCredits: vehicles.cost_in_credits,
-            speed: vehicles.speed,
+            speed: vehicles.max_atmosphering_speed,
             length: vehicles.length,
             crew: vehicles.crew,
             passengers: vehicles.passengers,
